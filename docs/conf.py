36eff23b4653b048750e5959a6382ed1a225ef73
mahi22151 <mahi22151@outlook.com>
2026-02-11 17:23:16 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,10 @@ release = '1.0'
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
+html_meta = {
+    "existing-meta": "value",
+    "msvalidate.01": "B807FE4A03DB636D50EF7962DDBBE11F",
+}
 
 html_theme = 'alabaster'
 html_static_path = ['_static']   
@@ -13,7 +17,3 @@ html_js_files = [
     'chatbot.js',
 ]
 
-html_meta = {
-    "existing-meta": "value",
-    "msvalidate.01": "B807FE4A03DB636D50EF7962DDBBE11F",
-}
